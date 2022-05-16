cc01f7a2696bb524fae0bd51811ae2efc3bfb97a
William Parsons <parwi096@student.otago.ac.nz>
2022-05-17 08:35:41 +1200

    added media

---
+++ AreaZoningSampleProject/Assets/Scripts/CameraController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+ * Simple camera controller, for demonstration purposes.
+ */
+public class CameraController : MonoBehaviour
+{
+    public float speed;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.A))
+            transform.Translate(Vector3.left * speed * Time.deltaTime, Space.World);
+        if (Input.GetKey(KeyCode.D))
+            transform.Translate(Vector3.right * speed * Time.deltaTime, Space.World);
+        if (Input.GetKey(KeyCode.W))
+            transform.Translate(Vector3.forward * speed * Time.deltaTime, Space.World);
+        if (Input.GetKey(KeyCode.S))
+            transform.Translate(Vector3.back * speed * Time.deltaTime, Space.World);
+    }
+}
